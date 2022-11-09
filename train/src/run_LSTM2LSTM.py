3b4c49da741f09530b7359f8a48a1da96c478a69
Maggie Liao <maggie16625@gmail.com>
2022-11-09 18:08:06 +0800

    a

--- train/src/run_LSTM2LSTM.py
+++ train/src/run_LSTM2LSTM.py
@@ -39,7 +39,7 @@ def data_preparation(hotel_id: int, date_feature: pd.DataFrame, cancel_target: p
     twn_covid_data['date'] = twn_covid_data['date'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d').strftime("%Y/%m/%d"))
     twn_covid_data.set_index('date', inplace=True)
 
-    covid_features_num = ['reproduction_rate'] #['new_cases', 'new_deaths']
+    covid_features_num = ['reproduction_rate','total_deaths_per_million'] #['new_cases', 'new_deaths']
 
 
     date_feature = date_feature.join(twn_covid_data[covid_features_num].fillna(0))
