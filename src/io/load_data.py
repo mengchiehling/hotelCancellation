aa346db1ae30afb38ae467aefcfde1059b2fd68a
Maggie Liao <maggie16625@gmail.com>
2023-04-19 14:11:57 +0800

    a

--- src/io/load_data.py
+++
@@ -1,9 +0,0 @@
-import os
-from datetime import datetime
-
-import numpy as np
-import pandas as pd
-
-from src.io.path_definition import get_file
-
-
--- train/api/eval_LSTM2LSTM.py
+++ train/api/eval_LSTM2LSTM.py
@@ -1,25 +1,18 @@
-import argparse
-import os
-from datetime import datetime
-
-import numpy as np
+from functools import partial
+from typing import Optional
+import argparse, os
 import pandas as pd
-from sklearn.metrics import mean_absolute_percentage_error
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
 
+from src import config
+from src.io.path_definition import get_file, load_yaml_file, get_datafetch
+from src.common.load_data import load_training_data
+from src.common.tools import timeseries_train_test_split
 from src.io.load_parameters import load_optimized_parameters
-from src.io.path_definition import get_file
-from train.logic.training_process import training_process
-from sklearn.preprocessing import LabelEncoder, MinMaxScaler
+from train.common.model_selection import cross_validation, model_training_pipeline
+from train.common.optimization_process import optimization_process
 
-hotel_info = pd.read_csv(get_file(os.path.join('data', 'cancel_dataset_hotel_info.csv')))
-
-cancel_target = pd.read_csv(get_file(os.path.join('data', 'cancel_dataset_target.csv')))
-date_feature = pd.read_csv(get_file(os.path.join('data', 'cancel_dataset_date_feature.csv')))
-hotel_meta = pd.read_csv(get_file(os.path.join('data', 'cancel_dataset_hotel_info.csv')), index_col=0)
-
-covid_data = pd.read_excel(get_file(os.path.join('data', 'owid-covid-data.xlsx'))
-                           #,engine='openpyxl'
-                           )
 
 
 def labelencoding(df, column: str):
@@ -30,102 +23,91 @@ def labelencoding(df, column: str):
     return df
 
 
-def data_preparation(hotel_id: int, date_feature: pd.DataFrame, cancel_target: pd.DataFrame
-                     # , smooth:bool=False, diff: Optional[List[int]]=None
-                     ):
+def create_dataset(dataset_: pd.DataFrame, test_size):
+    # 等同於網路上的train_test_split步驟
+    y = dataset_['label']
 
-    column = f"hotel_{hotel_id}_canceled"
+    df_hotel = dataset_.copy()
 
-    cancel_target['date'] = cancel_target['date'].apply(lambda x: datetime.strptime(x, '%Y/%m/%d').strftime("%Y/%m/%d"))
-    cancel_target.set_index('date', inplace=True)
+    train_dataset_, eval_dataset, train_target_, eval_target = timeseries_train_test_split(df_hotel)
 
-    hotel_cancel = cancel_target[column].replace("-", np.nan).dropna().astype(int)
+    return train_dataset_, eval_dataset, train_target_, eval_target
 
-    date_feature['date'] = date_feature['date'].apply(lambda x: datetime.strptime(x, '%Y/%m/%d').strftime("%Y/%m/%d"))
-    date_feature.set_index('date', inplace=True)
-    date_feature = date_feature.loc[hotel_cancel.index]
 
-    date_feature['canceled'] = hotel_cancel   # 原始值
-    #date_feature['canceled_label'] = hotel_cancel  # hotel_cancel_smooth.mean()
+def set_configuration():
 
-    twn_covid_data = covid_data[covid_data['iso_code'] == 'TWN']
-    twn_covid_data['date'] = twn_covid_data['date'].apply(
-        lambda x: datetime.strptime(x, '%Y-%m-%d').strftime("%Y/%m/%d"))
-    twn_covid_data.set_index('date', inplace=True)
+    config.hotel_id = args.hotel_id
+    config.configuration = args.configuration
+    config.algorithm = args.algorithm
+    config.n_splits = args.n_splits
+    config.test_size = args.test_size
+    config.max_train_size = args.max_train_size
+    config.input_range = args.input_range
+    config.prediction_time = args.prediction_time
+    config.weekly_inputs = args.weekly_inputs
 
-    covid_features_num = []  # ['new_cases', 'new_deaths']
-    covid_features_cat = []
 
-    date_feature = date_feature.join(twn_covid_data[covid_features_num+covid_features_cat].fillna(0))
+def to_timeseries_dataframe(df_: pd.DataFrame, idx_: pd.Series) -> pd.DataFrame:
 
-    num_feature_columns = ['canceled'] + covid_features_num
-    #num_feature_columns = ['canceled', 'canceled_label','days2vecation','vecation_days','Precp','PrecpHour','SunShine','Temperature']
+    """
 
-    #if smooth:
-        # Smoothed features for input
+    :param df_: PMS data
+    :param idx_: range of timestamps
+    :return:
+    """
 
-        #for window in [3, 14]:
-            #c  = f'canceled_{window}_roll'
-            #roll = hotel_cancel.rolling(window=window, min_periods=1)
-            #date_feature[c] = roll.mean()
-            #num_feature_columns.extend([c])
-        #date_feature['MACD'] = date_feature[f'canceled_3_roll'] - date_feature[f'canceled_14_roll']
-        #num_feature_columns.append('MACD')
+    # Simple time series, without extra features.
+    booking_feature = df_.groupby(by="check_in").agg(canceled=('label', 'sum'),
+                                                     booking=('label', 'count'),
+                                                     holiday=('holiday', np.unique))
 
-    #if diff is not None:
-        #for time_diff in diff:
-            #c = f'diff_{time_diff}'
-            #date_feature[c] = (date_feature['canceled'] - date_feature['canceled'].shift(time_diff)).fillna(0)
-            #num_feature_columns.append(c)
+    booking_feature = booking_feature.reindex(idx_, fill_value=0)
 
-    return num_feature_columns, covid_features_cat, date_feature
-
+    return booking_feature
 
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--input_range', type=int, help='length of input time series')
-    parser.add_argument('--prediction_time', type=int, help='length of output time series')
     parser.add_argument('--hotel_id', type=int, help='id of hotel, should exists in cancel_dataset_target.csv')
-    #parser.add_argument('--diff', type=int,  nargs='+', help='差分', default=[])
-    #parser.add_argument('--smooth', action='store_true')
+    parser.add_argument('--configuration', type=str, help='A')
+    parser.add_argument('--algorithm', type=str, help="LSTM2LSTM, CNN2LSTM")
+    parser.add_argument('--n_splits', type=int, default=5)
+    parser.add_argument('--test_size', type=int, default=28)
+    parser.add_argument('--max_train_size', type=int, default=180)
+    parser.add_argument('--input_range', type=int, default=28)
+    parser.add_argument('--prediction_time', type=int, default=7)
+    parser.add_argument('--weekly_inputs', action='store_true')
 
     args = parser.parse_args()
 
-    input_range = args.input_range
-    prediction_time = args.prediction_time
-    hotel_id = args.hotel_id
-    model_type = 'LSTM2LSTM'
-    #diff = args.diff
-    #smooth=args.smooth
+    set_configuration()
+
+    file_path = get_file(os.path.join('config', 'training_config.yml'))
+    metadata = load_yaml_file(file_path)
+
+    features_configuration = metadata['feature_configuration'][args.configuration]
+
+    config.categorical_features = features_configuration['categorical']
+    config.numerical_features = features_configuration['numerical']
+
+    df, idx = load_training_data(args.hotel_id, remove_business_booking=True)
+
+    for encoded_column in config.categorical_features:
+        df = labelencoding(df, encoded_column)
+
+    train_dataset, test_dataset, _, _ = create_dataset(df, test_size=args.test_size)
+
+    end_of_train_dataset = idx.index(train_dataset.iloc[-1]['check_in'])  # index
+
+    test_dataset = to_timeseries_dataframe(test_dataset, idx[:end_of_train_dataset+1])
+    # train_dataset = to_timeseries_dataframe(train_dataset, idx)
 
 
-    #已經找出最優化的參數組合，它會放入下面的training process，最後產出y_true,y_pred
-    params, _ = load_optimized_parameters(f"{hotel_id}_{model_type}" + "_logs_[\d]{8}-[\d]{2}.json")
-
-    params['batch_size'] = int(params['batch_size'])
-    params['decoder_dense_units'] = int(params['batch_size'])
-    params['encoder_lstm_units'] = int(params['encoder_lstm_units'])
-
-    n_splits = 7
-    test_size = 28
-
-    categorical_features =[] #['vecation', 'weekdate','season','midd','sallery', 'is_rest_day','s_vecation', 'w_vecation','workingday','is_event','cov_policy']  # encoded_columns + nonencoded_columns
-
-    #之後有類別型特徵時需要做labelencoding
-    #for encoded_column in categorical_features:
-        #date_feature = labelencoding(date_feature, encoded_column)
 
 
-    # 做training或evaluation都要讀取數據
-    numerical_features, covid_features_cat, date_feature = data_preparation(hotel_id, date_feature, cancel_target
-                                                        # , smooth=smooth, diff=diff
-                                                        )
 
-    categorical_features = categorical_features + covid_features_cat
-    date_feature = date_feature[numerical_features+categorical_features]
 
     y_true, y_pred = training_process(input_range=input_range, prediction_time=prediction_time,
                                       date_feature=date_feature, numerical_features=numerical_features,categorical_features=categorical_features,
